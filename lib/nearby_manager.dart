965b4badd5434406661cb6c4b8d110b9423a8ba9
Thomas Demoulin <thomasdem07@yahoo.be>
2022-06-06 15:08:33 +0200

    Fix bug

--- lib/nearby_manager.dart
+++ lib/nearby_manager.dart
@@ -198,9 +198,9 @@ class AdhocManager extends ChangeNotifier {
 
         var entry =
             _globalPlaylist[data['uuid']] ?? HashMap<String, PlatformFile>();
-        entry.putIfAbsent(
+        entry.update(
             name,
-            () => PlatformFile(
+            (value) => PlatformFile(
                 bytes: song,
                 name: name,
                 path: tempFile.path,
